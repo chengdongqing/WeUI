8b95675f6ecae4dfadab96eac8d800ca58591c64
James Lu <1912525497@qq.com>
2024-02-25 21:52:07 +0800

    优化已知问题

--- app/src/main/kotlin/top/chengdongqing/weui/ui/components/swiper/Swiper.kt
+++ app/src/main/kotlin/top/chengdongqing/weui/ui/components/swiper/Swiper.kt
@@ -15,15 +15,17 @@ import androidx.compose.foundation.pager.PagerState
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.yield
+import kotlinx.coroutines.launch
+import java.util.Timer
+import kotlin.concurrent.timerTask
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
@@ -35,15 +37,31 @@ fun <T> WeSwiper(
     },
     autoplay: Boolean = true,
     interval: Long = 3000,
-    showDots: Boolean = true,
+    indicator: (@Composable BoxScope.(count: Int, current: Int) -> Unit)? = { count, current ->
+        WeSwiperDefaults.Indicator(
+            count,
+            current,
+            Modifier
+                .align(Alignment.BottomCenter)
+                .offset(y = (-10).dp)
+        )
+    },
     content: @Composable (PagerScope.(T) -> Unit)
 ) {
-    LaunchedEffect(autoplay) {
-        while (autoplay) {
-            yield() // 在长时间运行的协程中，使用 yield() 可以确保协程不会长时间占用线程，从而提高程序的性能和响应性
-            delay(interval)
-            val targetPage = (pagerState.currentPage + 1) % pagerState.pageCount
-            pagerState.animateScrollToPage(targetPage)
+    if (autoplay) {
+        val coroutineScope = rememberCoroutineScope()
+        DisposableEffect(pagerState.currentPage) {
+            val timer = Timer()
+            timer.schedule(timerTask {
+                coroutineScope.launch {
+                    val targetPage = (pagerState.currentPage + 1) % options.size
+                    pagerState.animateScrollToPage(targetPage)
+                }
+            }, interval, interval)
+
+            onDispose {
+                timer.cancel()
+            }
         }
     }
 
@@ -56,34 +74,34 @@ fun <T> WeSwiper(
             content(options[index])
         }
 
-        if (showDots) {
-            Indicator(options.size, pagerState.currentPage)
+        if (indicator != null) {
+            indicator(options.size, pagerState.currentPage)
         }
     }
 }
 
-@Composable
-private fun BoxScope.Indicator(count: Int, index: Int) {
-    Row(
-        Modifier
-            .align(Alignment.BottomCenter)
-            .offset(y = (-10).dp)
-            .width((count * 16).dp)
-            .height(3.dp)
-            .clip(RoundedCornerShape(2.dp))
-            .background(Color.White.copy(alpha = 0.2f))
-    ) {
-        val offsetX by animateDpAsState(
-            targetValue = (index * 16).dp,
-            label = "OffsetAnimation"
-        )
-        Box(
-            Modifier
-                .width(16.dp)
+object WeSwiperDefaults {
+    @Composable
+    fun Indicator(count: Int, current: Int, modifier: Modifier = Modifier) {
+        Row(
+            modifier
+                .width((count * 16).dp)
                 .height(3.dp)
-                .offset(x = offsetX)
                 .clip(RoundedCornerShape(2.dp))
-                .background(Color.White)
-        )
+                .background(Color.White.copy(alpha = 0.2f))
+        ) {
+            val offsetX by animateDpAsState(
+                targetValue = (current * 16).dp,
+                label = "OffsetAnimation"
+            )
+            Box(
+                Modifier
+                    .width(16.dp)
+                    .height(3.dp)
+                    .offset(x = offsetX)
+                    .clip(RoundedCornerShape(2.dp))
+                    .background(Color.White)
+            )
+        }
     }
 }
