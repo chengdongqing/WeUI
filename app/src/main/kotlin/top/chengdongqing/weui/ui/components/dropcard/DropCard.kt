775626bc29d0781fd8517492bc4eef6253bb3384
James Lu <1912525497@qq.com>
2024-03-02 14:34:18 +0800

    优化已知问题

--- app/src/main/kotlin/top/chengdongqing/weui/ui/components/dropcard/DropCard.kt
+++ app/src/main/kotlin/top/chengdongqing/weui/ui/components/dropcard/DropCard.kt
@@ -8,7 +8,6 @@ import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
@@ -28,30 +27,10 @@ fun <T> WeDropCard(
     onSwiped: (Int, T) -> Unit,
     content: @Composable BoxScope.(T) -> Unit
 ) {
-    val animatedOffsets = remember(items.size) { items.map { Animatable(0f) } }
-    val coroutineScope = rememberCoroutineScope()
-
     Box {
         items.reversed().forEachIndexed { index, item ->
-            // 判断当前卡片是否在顶部三张卡片中
             val isInTopThree = index > items.lastIndex - 3
             val current = if (items.lastIndex >= 3) index - 3 else index
-
-            LaunchedEffect(items.size) {
-                if (isInTopThree) {
-                    // 动画到指定的偏移量
-                    animatedOffsets[index].animateTo(
-                        targetValue = (64 - current * 32f),
-                        animationSpec = tween(800)
-                    )
-                } else {
-                    // 不在顶部的卡片，设置一个默认偏移量
-                    animatedOffsets[index].animateTo(
-                        targetValue = 0f,
-                        animationSpec = tween(800)
-                    )
-                }
-            }
 
             CardItem(
                 key = item,
@@ -59,7 +38,7 @@ fun <T> WeDropCard(
                     .then(
                         if (isInTopThree) {
                             Modifier
-                                .offset(y = animatedOffsets[index].value.dp)
+                                .offset(y = (64 - current * 32f).dp)
                                 .scale(1 - 0.05f * (2 - current))
                         } else {
                             Modifier.scale(0.5f)
@@ -67,9 +46,6 @@ fun <T> WeDropCard(
                     ),
                 onSwiped = {
                     onSwiped(index, item)
-                    coroutineScope.launch {
-                        animatedOffsets[index].snapTo(0f)
-                    }
                 }
             ) {
                 content(item)
@@ -85,8 +61,8 @@ private fun <T> CardItem(
     onSwiped: () -> Unit,
     content: @Composable () -> Unit
 ) {
-    val screenWidth = LocalConfiguration.current.screenWidthDp.dp
-    val swipeOffset = (screenWidth.value * 3)
+    val screenWidth = LocalConfiguration.current.screenWidthDp
+    val swipeOffset = (screenWidth * 3)
     val swipeX = remember(key) { Animatable(0f) }
     val swipeY = remember(key) { Animatable(0f) }
     val coroutineScope = rememberCoroutineScope()
@@ -97,7 +73,7 @@ private fun <T> CardItem(
                 .graphicsLayer {
                     translationX = swipeX.value
                     translationY = swipeY.value
-                    rotationZ = (swipeX.value / screenWidth.value * 12).coerceIn(-40f, 40f)
+                    rotationZ = (swipeX.value / screenWidth * 12).coerceIn(-40f, 40f)
                 }
                 .pointerInput(Unit) {
                     detectDragGestures(
@@ -118,9 +94,9 @@ private fun <T> CardItem(
                                     }
                                 } else {
                                     if (swipeX.targetValue > 0) {
-                                        swipeX.animateTo(swipeOffset, tween(200))
+                                        swipeX.animateTo(swipeOffset.toFloat(), tween(200))
                                     } else {
-                                        swipeX.animateTo(-swipeOffset, tween(200))
+                                        swipeX.animateTo(-swipeOffset.toFloat(), tween(200))
                                     }
                                     onSwiped()
                                 }
--- app/src/main/kotlin/top/chengdongqing/weui/ui/screens/basic/DropCard.kt
+++ app/src/main/kotlin/top/chengdongqing/weui/ui/screens/basic/DropCard.kt
@@ -1,16 +1,15 @@
 package top.chengdongqing.weui.ui.screens.basic
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import coil.compose.AsyncImage
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 import top.chengdongqing.weui.ui.components.dropcard.WeDropCard
 import top.chengdongqing.weui.ui.components.screen.WeScreen
 
@@ -27,10 +26,10 @@ fun DropCardScreen() {
                 "https://s.xiaopeng.com/xp-fe/mainsite/2023/g6/v1_5/p5-4-1.jpg",
                 "https://s1.xiaomiev.com/activity-outer-assets/web/su7/1-3_m.jpg",
                 "https://s.xiaopeng.com/xp-fe/mainsite/2023/g92024/v2/Gu72d34fs245/m/p3-1.jpg",
-                "https://p.ampmake.com/lilibrary/68886087878537/aa66bf58-8578-4a5c-90cf-bae0fc1847dd.jpg@d_progressive"
+                "https://p.ampmake.com/lilibrary/787536779512770/551b13df-1fe3-43a0-9049-f4cff1de370f.jpg",
+                "https://p.ampmake.com/lilibrary/688863524210496/fe2ee1c6-5c94-420a-bee0-7bb040f84f03.jpg"
             )
         }
-        val coroutineScope = rememberCoroutineScope()
 
         WeDropCard(
             items,
@@ -38,18 +37,17 @@ fun DropCardScreen() {
                 .fillMaxWidth()
                 .fillMaxHeight(0.8f),
             onSwiped = { _, item ->
-                coroutineScope.launch {
-                    items.remove(item)
-                    delay(200)
-                    items.add(item)
-                }
+                items.remove(item)
+                items.add(item)
             }
         ) { item ->
             AsyncImage(
                 model = item,
                 contentDescription = null,
                 contentScale = ContentScale.Crop,
-                modifier = Modifier.matchParentSize()
+                modifier = Modifier
+                    .matchParentSize()
+                    .background(Color.Black)
             )
         }
     }
