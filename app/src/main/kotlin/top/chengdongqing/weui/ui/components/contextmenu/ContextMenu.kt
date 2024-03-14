27c135f3cfdca104c9e40cec1c63a5ca16157680
James Lu <1912525497@qq.com>
2024-03-14 22:15:05 +0800

    完善ContextMenu

--- app/src/main/kotlin/top/chengdongqing/weui/ui/components/contextmenu/ContextMenu.kt
+++ app/src/main/kotlin/top/chengdongqing/weui/ui/components/contextmenu/ContextMenu.kt
@@ -2,18 +2,19 @@ package top.chengdongqing.weui.ui.components.contextmenu
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Stable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
@@ -22,85 +23,112 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Popup
-import top.chengdongqing.weui.ui.components.divider.WeDivider
-import top.chengdongqing.weui.ui.theme.FontColorLight
-import top.chengdongqing.weui.utils.toIntOffset
 
 @Composable
-fun WeContextMenu(options: List<String>) {
-    var showPopup by remember { mutableStateOf(false) }
-    var offset by remember { mutableStateOf(IntOffset.Zero) }
-
-    LazyColumn(
-        modifier = Modifier
-            .clip(RoundedCornerShape(4.dp))
-            .background(Color.White)
-            .pointerInput(Unit) {
-                detectTapGestures(onLongPress = {
-                    offset = it.toIntOffset()
-                    showPopup = true
-                })
-            }
-    ) {
-        items(20) { index ->
-            ListItem("item${index + 1}")
-        }
-    }
-
-    if (showPopup) {
-        Popup(offset = offset) {
-            Column(
-                modifier = Modifier
-                    .width(180.dp)
-                    .shadow(8.dp)
-                    .clip(RoundedCornerShape(2.dp))
-                    .background(Color.White)
-            ) {
-                options.forEach {
-                    MenuItem(onClick = {}) {
-                        Text(text = it)
-                    }
+fun WeContextMenu(
+    position: IntOffset,
+    options: List<String>,
+    onCancel: () -> Unit,
+    onTap: (index: Int) -> Unit
+) {
+    Popup(offset = position, onDismissRequest = onCancel) {
+        Column(
+            modifier = Modifier
+                .width(180.dp)
+                .shadow(8.dp)
+                .clip(RoundedCornerShape(2.dp))
+                .background(Color.White)
+        ) {
+            options.forEachIndexed { index, item ->
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .heightIn(56.dp)
+                        .clickable { onTap(index) }
+                        .padding(horizontal = 12.dp),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(text = item, color = MaterialTheme.colorScheme.onPrimary, fontSize = 17.sp)
                 }
             }
         }
     }
 }
 
-@Composable
-private fun ListItem(content: String) {
-    Row(
-        modifier = Modifier
-            .heightIn(56.dp)
-            .padding(12.dp),
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        Text(
-            text = content,
-            modifier = Modifier.weight(1f),
-            color = FontColorLight,
-            fontSize = 14.sp,
-            fontWeight = FontWeight.Bold
-        )
-    }
-    WeDivider()
+@Stable
+interface ContextMenuState {
+    /**
+     * 是否显示
+     */
+    val visible: Boolean
+
+    /**
+     * 偏移位置
+     */
+    val position: IntOffset
+
+    /**
+     * 菜单列表
+     */
+    val options: List<String>
+
+    /**
+     * 当前被点击项的在列表中的索引
+     */
+    val listIndex: Int
+
+    /**
+     * 显示菜单
+     */
+    fun show(position: IntOffset, options: List<String>, currentIndex: Int)
+
+    /**
+     * 隐藏菜单
+     */
+    fun hide()
 }
 
 @Composable
-private fun MenuItem(onClick: () -> Unit, content: @Composable () -> Unit) {
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .heightIn(56.dp)
-            .clickable { onClick() }
-            .padding(horizontal = 12.dp),
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        content()
+fun rememberContextMenuState(onTap: (listIndex: Int, menuIndex: Int) -> Unit): ContextMenuState {
+    val state = remember { ContextMenuStateImpl() }
+
+    if (state.visible) {
+        WeContextMenu(
+            position = state.position,
+            options = state.options,
+            onCancel = { state.hide() },
+        ) { menuIndex ->
+            onTap(state.listIndex, menuIndex)
+            state.hide()
+        }
     }
+
+    return state
+}
+
+class ContextMenuStateImpl : ContextMenuState {
+    override val visible: Boolean get() = _visible
+    override val position: IntOffset get() = _position
+    override val options: List<String> get() = _options
+    override val listIndex: Int get() = _listIndex
+
+    override fun show(position: IntOffset, options: List<String>, currentIndex: Int) {
+        _listIndex = currentIndex
+        _position = position
+        _options = options
+        _visible = true
+    }
+
+    override fun hide() {
+        _visible = false
+    }
+
+    private var _visible by mutableStateOf(false)
+    private var _position by mutableStateOf(IntOffset.Zero)
+    private var _options by mutableStateOf<List<String>>(emptyList())
+    private var _listIndex by mutableIntStateOf(0)
 }
--- app/src/main/kotlin/top/chengdongqing/weui/ui/screens/feedback/ContextMenu.kt
+++ app/src/main/kotlin/top/chengdongqing/weui/ui/screens/feedback/ContextMenu.kt
@@ -1,10 +1,27 @@
 package top.chengdongqing.weui.ui.screens.feedback
 
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.layout.Box
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import top.chengdongqing.weui.ui.components.contextmenu.WeContextMenu
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.layout.positionInParent
+import top.chengdongqing.weui.ui.components.cardlist.WeCardList
+import top.chengdongqing.weui.ui.components.cardlist.WeCardListItem
+import top.chengdongqing.weui.ui.components.contextmenu.rememberContextMenuState
+import top.chengdongqing.weui.ui.components.dialog.DialogOptions
+import top.chengdongqing.weui.ui.components.dialog.rememberWeDialog
 import top.chengdongqing.weui.ui.components.screen.WeScreen
+import top.chengdongqing.weui.utils.toIntOffset
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun ContextMenuScreen() {
     WeScreen(
@@ -13,9 +30,33 @@ fun ContextMenuScreen() {
         scrollEnabled = false
     ) {
         val menus = remember {
-            listOf("菜单1", "菜单2", "菜单3", "菜单4")
+            listOf("标为未读", "置顶该聊天", "不显示该聊天", "删除该聊天")
+        }
+        val dialog = rememberWeDialog()
+        val contextMenuState = rememberContextMenuState { listIndex, menuIndex ->
+            dialog.show(
+                DialogOptions(
+                    title = "您点击的是第${listIndex + 1}项的“${menus[menuIndex]}”",
+                    onCancel = null
+                )
+            )
         }
 
-        WeContextMenu(menus)
+        WeCardList {
+            items(30) { index ->
+                var offset by remember { mutableStateOf(Offset.Zero) }
+                Box(modifier = Modifier
+                    .onGloballyPositioned {
+                        offset = it.positionInParent()
+                    }
+                    .pointerInput(Unit) {
+                        detectTapGestures(onLongPress = {
+                            contextMenuState.show((offset + it).toIntOffset(), menus, index)
+                        })
+                    }) {
+                    WeCardListItem(label = "第${index + 1}项")
+                }
+            }
+        }
     }
 }
