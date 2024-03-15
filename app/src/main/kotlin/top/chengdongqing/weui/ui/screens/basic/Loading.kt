8089e4e83b271a519ada4359ed3f753f8ec758b5
James Lu <1912525497@qq.com>
2024-03-15 11:32:22 +0800

    完善Loading

--- app/src/main/kotlin/top/chengdongqing/weui/ui/screens/basic/Loading.kt
+++ app/src/main/kotlin/top/chengdongqing/weui/ui/screens/basic/Loading.kt
@@ -1,5 +1,6 @@
 package top.chengdongqing.weui.ui.screens.basic
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.width
@@ -7,13 +8,21 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import top.chengdongqing.weui.ui.components.loading.DotDanceLoading
+import top.chengdongqing.weui.ui.components.loading.MiLoadingMobile
+import top.chengdongqing.weui.ui.components.loading.MiLoadingWeb
 import top.chengdongqing.weui.ui.components.loading.WeLoading
+import top.chengdongqing.weui.ui.components.loading.WeLoadingMP
 import top.chengdongqing.weui.ui.components.screen.WeScreen
 import top.chengdongqing.weui.ui.theme.PrimaryColor
 
 @Composable
 fun LoadingScreen() {
-    WeScreen(title = "Loading", description = "加载中") {
+    WeScreen(
+        title = "Loading",
+        description = "加载中",
+        verticalArrangement = Arrangement.spacedBy(40.dp)
+    ) {
         Row(verticalAlignment = Alignment.CenterVertically) {
             WeLoading()
             Spacer(Modifier.width(24.dp))
@@ -23,5 +32,10 @@ fun LoadingScreen() {
             Spacer(Modifier.width(24.dp))
             WeLoading(size = 32.dp, color = PrimaryColor)
         }
+
+        WeLoadingMP()
+        MiLoadingWeb()
+        MiLoadingMobile()
+        DotDanceLoading()
     }
 }
